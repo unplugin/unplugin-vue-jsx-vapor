eb5ee018aed8d121adae6bc00668303cbb09f3de
gaokefei <260480378@qq.com>
2024-07-04 03:20:00 +0800

    chore: update playground

--- playground/slot.vue
+++ playground/slot.vue
@@ -1,15 +1,36 @@
 <script lang="tsx">
-import { defineComponent } from 'vue'
+import { defineComponent, ref } from 'vue'
 
 const Comp = (props, { slots }) => (
-  <>{slots.default ? <slots.default /> : <div>default slot</div>}</>
+  <>
+    {slots.default ? (
+      <slots.default foo={props.foo} />
+    ) : (
+      <div>default slot</div>
+    )}
+  </>
 )
 
-let slots = { default: () => <div>foo</div> }
+let slots = {
+  default: (scope) => <div>{scope.foo}</div>,
+}
 
 export default defineComponent({
   setup(props) {
-    return <Comp v-slots={slots}></Comp>
+    const foo = ref('foo')
+    return (
+      <>
+        <input
+          value={foo.value}
+          onInput={(e) => (foo.value = e.target.value)}
+        />
+        <Comp v-slots={slots} foo={foo.value} />
+        <Comp
+          v-slots={{ default: ({ foo }) => <div>{foo}</div> }}
+          foo={foo.value}
+        />
+      </>
+    )
   },
 })
 </script>
